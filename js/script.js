ea851d60cb37ca8a9bd7cad1a01ce9f3a8a84560
Alexis Arevalo <alexgian1997@gmail.com>
2021-09-27 11:09:22 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var menu = document.querySelector("#mainMenu .menu");
+    menu.classList.toggle("show-small");
+    if(menu.classList.toggle("show-small") === true){
+        
+    }
+}
